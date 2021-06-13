108529c2261c50fbd03c0e18ff50f205dcb57a2f
prakash thapa <pt6769759@gmail.com>
2021-06-13 07:25:27 +0545

    modified

--- app.php
+++ app.php
@@ -5,21 +5,69 @@ use DateTime;
 
 
 class Determine{
-
-public function canOver($a=''){
+//balancer
+public function canOver($a=''):string{
 if($a < 500 || $a > 5000 ){
  return "value shoule between 500-5000";
     }
 
 }
 
-public function setTime($t=''){
+//('2021/01/02')
+public function setTime($t=''):string{
 $now = new DateTime($t);
 return $now->format('d/m/y');
 
-   }
+}
+
+//inhouse/company
+public function getWorkframe($frame=''):string{
+return $frame;
+}
+
+
+//category { category may be any kind from array }
+public function getCategory($a):array{
+return $a;   
+}
+
+//strength {strengh is measured on 0-100}
+public function setStrength($r):int{
+
+if(!is_numeric($r) || $r > 100){
+   return 500;
+    }else{
+   return $r;
+  }
+}
+
+
+//determinde duration
+public function setValidation($val){
+ if(intval($val) > 30){
+  return "Expired";
+ }else{
+   return "Must be Number";
+ }
+}
+
+
 
 }
+
+
+print_r(Determine::setValidation(3000));
+
+/*
+
+timing
+work
+balance
+category
+strength
+validate
+
+*/
 
 
 ?>
